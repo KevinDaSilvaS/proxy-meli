cf780a1dd616466cb6fedec4bfc47459e11d6c45
Kevin <kevin.kds80@gmail.com>
2023-03-23 12:07:28 -0300

    add rate limiting rules bussiness logic

---
+++ app/rules/rules.js
@@ -0,0 +1,44 @@
+const { commonResponses } = require('../../helpers/response')
+
+const supportedRuleTypes = {
+    ip: "ip",
+    path: "path"
+}
+
+const getRule = async (type, repository) => {
+    return await repository.db.getOneSearch({type}, "rules", repository.dbConn)
+}
+
+const addRule = async (rule, repository) => {    
+    const { type } = rule
+
+    const supportedType = supportedRuleTypes[type]
+
+    if (!supportedType) {
+        return commonResponses.rule_type_not_supported
+    }
+
+    const existingRule = await getRule(type, repository)
+    if (existingRule) {
+        return commonResponses.rule_type_already_exists
+    }
+
+    const createdRule = await repository.db.create(
+        rule, 
+        "rules", 
+        repository.dbConn)
+
+    return { status: 201, body: createdRule } 
+}
+
+const removeRule = async (type, repository) => {
+    await repository.db.remove({type}, "rules", repository.dbConn)
+    return { status: 204, body: {} }
+}
+
+module.exports = {
+    getRule,
+    removeRule,
+    addRule,
+    supportedRuleTypes
+}
