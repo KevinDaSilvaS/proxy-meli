ca25e4d0957c584c1a93ef7e10c901ac6e719b96
Kevin <kevin.kds80@gmail.com>
2023-03-23 12:08:13 -0300

    add proxy bussiness logic

---
+++ app/proxy/call-api.js
@@ -0,0 +1,8 @@
+const callApi = requestObject => {
+    const {url, path, headers, verb, body} = requestObject
+    //console.log(url, path, headers, verb, body)
+}
+
+module.exports = {
+    callApi
+}
---
+++ app/proxy/proxy.js
@@ -0,0 +1,79 @@
+const { callApi } = require('./call-api')
+const { getRule, supportedRuleTypes } = require('../rules/rules')
+const { addMetric } = require('../monitoring/metrics')
+const { commonResponses } = require('../../helpers/response')
+
+const createRequestObject = req => ({
+    url: 'oi',
+    path: req.path,
+    ip: req.ip,
+    headers: req.headers,
+    method: req.method,
+    body: req.body
+})
+
+const proxy = async (req, repository) => {
+    const reqObject = createRequestObject(req)
+
+    const ip = supportedRuleTypes.ip
+    const ruleIp = await getRule(ip, repository)
+    const ipKey = reqObject[ip]
+    const reachedMaxRequestsByIp =
+        await checkMaxRequests(repository, ipKey, ruleIp)
+
+    const path = supportedRuleTypes.path
+    const rulePath = await getRule(path, repository)
+    const pathKey = reqObject[path]
+    const reachedMaxRequestsByPath =
+        await checkMaxRequests(repository, pathKey, rulePath)
+
+    if (reachedMaxRequestsByIp) {
+        addMetric(reqObject, repository)
+        return commonResponses.max_requests_reached
+    }
+        
+
+    if (reachedMaxRequestsByPath) {
+        addMetric(reqObject, repository)
+        return commonResponses.max_requests_reached
+    }  
+
+    if (ruleIp) 
+        await addRequestCount(repository, ipKey, ruleIp._source.expiration_every)
+
+    if (rulePath) 
+        await addRequestCount(repository, pathKey, rulePath._source.expiration_every)
+
+    callApi(reqObject)
+    addMetric(reqObject, repository)
+    return { status: 200, body: { api: 'success' } }
+}
+
+const getRequestCount = async (repository, key) => {
+    const requests = await repository.cache.getCache(
+        repository.cacheConn,
+        key
+    ) || 0
+
+    return parseInt(requests)
+}
+
+const addRequestCount = async (repository, key, ttl) => {
+    return await repository.cache.putCache(
+        repository.cacheConn,
+        key,
+        await getRequestCount(repository, key) + 1,
+        ttl
+    )
+}
+
+const checkMaxRequests = async (repository, key, rule) => {
+    const totalRequests = await getRequestCount(repository, key)
+    if (rule && totalRequests >= rule._source.max_requests) {
+        return true
+    }
+}
+
+module.exports = {
+    proxy
+}
