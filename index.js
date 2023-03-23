8530e0070a47fe57304188abe7b91390fffb5dfc
Kevin <kevin.kds80@gmail.com>
2023-03-23 12:09:59 -0300

    add web server starting point on index

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const rules = require('./app/rules/rules')
+const metrics = require('./app/monitoring/metrics')
+const proxy = require('./app/proxy/proxy')
+const routes = require('./config/routes')
+const cacheRepository = require('./repository/cache-repository')
+const dbRepository = require('./repository/elasticsearch-repository')
+
+const cacheConn = cacheRepository.connect()
+const dbConn = dbRepository.conn()
+
+const repository = {
+  cacheConn,
+  dbConn,
+  db: dbRepository,
+  cache: cacheRepository
+}
+
+app.use(express.json())
+
+app.use(async (req, res, next) => {
+  const startPath = req.path.split('/')[1]
+
+  if (routes[startPath]) {
+    return next()
+  }
+
+  const {status, body} = await proxy.proxy(req, repository)
+  return res.status(status).send(body)
+})
+
+app.post('/rules', async (req, res) => {
+  const {status, body} = await rules.addRule(req.body, repository)
+  res.status(status).send(body)
+})
+
+app.delete('/rules/:type', async (req, res) => {
+  const {status, body} = await rules.removeRule(req.params.type, repository)
+  res.status(status).send(body)
+})
+
+app.get('/metrics', async (req, res) => {
+  const {status, body} = metrics.getMetrics("filters", true)
+  res.status(status).send(body)
+})
+
+app.listen(port, async () => {
+  repository.dbConn = await dbConn
+  repository.cacheConn = await cacheConn
+  console.log(`Example app listening on port ${port}`)
+})
