0d6f5cfd6aef8504aa77c2e32dc3d9500d0c09f2
Kevin <kevin.kds80@gmail.com>
2023-03-24 15:42:06 -0300

    add sanitation and support to query parameters

--- app/proxy/call-api.js
+++ app/proxy/call-api.js
@@ -2,20 +2,30 @@ const axios = require('axios')
 const { createResponse } = require('../../helpers/response')
 
 const callApi = async requestObject => {
-    const { url, path, headers, method, body } = requestObject
+    const { url, path, headers, method, body, query } = requestObject
+    const queryStr = buildQuery(query)
+    const apiUrl = `${url}${path}?${queryStr}`
 
     try {
         const res = await axios({
             method: method.toLowerCase(),
-            url: `${url}${path}`,
+            url: apiUrl,
             data: { ...body },
-            headers
+            headers: sanitizeHeaders(headers)
         })
 
         return createResponse(200, res.data, res.headers)
     } catch (error) {
         return createResponse(500, { error: error.message })
     }
+}
+
+const buildQuery = query => Object.keys(query)
+    .reduce((acc, key) => `${acc}&${key}=${query[key]}`, ``)
+
+const sanitizeHeaders = headers => {
+    ["host", "cookie", "accept"].map(key => delete headers[key])
+    return headers
 }
 
 module.exports = {
