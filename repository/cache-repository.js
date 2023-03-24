cf666519acef9cb613ab5383a606aa993c8ae6ba
Kevin <kevin.kds80@gmail.com>
2023-03-24 13:44:33 -0300

    add more env vars

--- repository/cache-repository.js
+++ repository/cache-repository.js
@@ -1,6 +1,8 @@
 const Redis = require("ioredis")
 
-const connect = async () => await new Redis()
+const connect = async () => await new Redis({
+    host: process.env.REDIS_HOST
+})
 
 const putCache = async (conn, key, value, ttl) => await conn.setex(key, ttl, value)
 
